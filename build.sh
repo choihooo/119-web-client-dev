7b1c8b6cca0b2bbb17b6e621385b44a2dfc92d68
choihooo <hochoi8621@naver.com>
2026-01-07 04:11:28 +0000

    fix:  빌드 스크립트 npmrc 복사 추가

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ mkdir -p output
 cp -R ./* ./output/ 2>/dev/null || true
 cp -R ./.github ./output/ 2>/dev/null || true
 cp -R ./.husky ./output/ 2>/dev/null || true
+# .npmrc.example을 복사하여 preinstall 스크립트가 작동하도록 함
+if [ -f .npmrc ]; then
+  cp .npmrc ./output/.npmrc 2>/dev/null || true
+fi
 rm -rf ./output/.git
 rm -rf ./output/output
 rm -rf ./output/node_modules
